8db30994f82e483471543478e34e922a69510056
Jochen Lillich <jochen@lillich.info>
2015-05-21 15:32:33 +0100

    Revert "chmod run.sh in Dockerfile"
    This reverts commit f6141767afd7ec6eceda39bd554a471adcd06311.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod 755 /setup/init/*.sh /setup/init/*/*.sh
+chmod 755 /run.sh /setup/init/*.sh /setup/init/*/*.sh
 
 for initscript in /setup/init/*.sh; do
   echo "=> Executing $initscript"
